943af8650aaf236cf8345cd0bcc7d0c240732b1a
Sophie Berger <sophie.berger@tailored-apps.com>
2020-01-11 15:57:31 -0800

    Exercise component created

---
+++ src/components/UI/Exercise/Exercise.js
@@ -0,0 +1,18 @@
+import react from 'react';
+import classes from './Exercise.css';
+
+class Exercise extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Exercise;
