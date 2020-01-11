a946637a4881d5092a6a9aaf0876f5624270c490
Adel <adel-s@outlook.com>
2020-01-11 15:33:57 -0800

    Setup boiler plate

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Layout from './hoc/Layout/Layout';
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <Layout>
+          <Switch>
+            <Route path="/" exact component={null} />
+          </Switch>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './NavigationItems.css';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const navigationItems = () => (
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link="/" exact>Home</NavigationItem>
+    </ul>
+);
+
+export default navigationItems;
