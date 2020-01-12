207181b76ad8ca3baaf58f8b141162180cbe7390
Sophie Berger <sophie.berger@tailored-apps.com>
2020-01-11 20:05:55 -0800

    Feedback received

--- src/components/UI/EmotionScale/EmotionScale.js
+++ src/components/UI/EmotionScale/EmotionScale.js
@@ -2,33 +2,32 @@ import React, { Component } from "react";
 import { Avatar } from "antd";
 
 class EmotionScale extends Component {
-  showThanks = () => {
-    console.log("Clicked");
-  };
+  state = { feedbackGiven: false };
+
   render() {
     return (
       <div>
         {" "}
         <Avatar
-          onClick={this.showThanks}
+          onClick={this.props.function}
           size={100}
           style={{ backgroundColor: "#E4300A" }}
           icon="user"
         />{" "}
         <Avatar
-          onClick={this.showThanks}
+          onClick={this.props.function}
           size={100}
           style={{ backgroundColor: "#f56a00" }}
           icon="user"
         />{" "}
         <Avatar
-          onClick={this.showThanks}
+          onClick={this.props.function}
           size={100}
           style={{ backgroundColor: "#ECE50B" }}
           icon="user"
         />{" "}
         <Avatar
-          onClick={this.showThanks}
+          onClick={this.props.function}
           size={100}
           style={{ backgroundColor: "#0CB404" }}
           icon="user"
