0301cd03521b3a7f71da6e476ee10a7e4e610e1d
Sophie Berger <sophie.berger@tailored-apps.com>
2020-01-11 18:00:53 -0800

    emotion scale

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Exercise from "../../components/UI/Exercise/Exercise";
+import EmotionScale from "../../components/UI/EmotionScale/EmotionScale";
 
 class Home extends Component {
   render() {
@@ -7,6 +8,7 @@ class Home extends Component {
       <div>
         <h1>Welcome to Home Page</h1>
         <Exercise title ={"Title"} description ={"desrciption"} source={"https://www.saebo.com/wp-content/uploads/2015/10/elbowstretch-1.png"}/>
+        <EmotionScale />
       </div>
     );
   }
