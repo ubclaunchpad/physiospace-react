5e7a1f3014cfdbc398d075e931b247fa0c22c2f7
Sophie Berger <sophie.berger@tailored-apps.com>
2020-01-11 18:41:30 -0800

    commit

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
+
 import Exercise from "../../components/UI/Exercise/Exercise";
-import EmotionScale from "../../components/UI/EmotionScale/EmotionScale";
+import FeedbackModal from "../../components/UI/FeedbackModal/FeedbackModal";
 
 class Home extends Component {
   render() {
     return (
       <div>
         <h1>Welcome to Home Page</h1>
-        <Exercise title ={"Title"} description ={"desrciption"} source={"https://www.saebo.com/wp-content/uploads/2015/10/elbowstretch-1.png"}/>
-        <EmotionScale />
+        <Exercise
+          title={"Title"}
+          description={"desrciption"}
+          source={
+            "https://www.saebo.com/wp-content/uploads/2015/10/elbowstretch-1.png"
+          }
+        />
+        <FeedbackModal />
       </div>
     );
   }
