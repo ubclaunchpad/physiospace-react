06399c5ca908b980ce76f2accf0a205710d96b45
Sophie Berger <sophie.berger@tailored-apps.com>
2020-01-11 19:25:50 -0800

    Clickable

--- src/components/UI/Exercise/Exercise.js
+++ src/components/UI/Exercise/Exercise.js
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
 import { Card } from "antd";
-import classes from "./Exercise.css";
 
 const { Meta } = Card;
 
 class Exercise extends Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
-      <div style={{width: 300, height: 400 }}>
-
+      <div style={{ width: 300, height: 400 }}>
         <Card hoverable cover={<img alt="example" src={this.props.source} />}>
           <Meta title={this.props.title} description={this.props.description} />
         </Card>
